18b953b802579376f66686aee8331a3e503a0b57
Brian Bidulock <bidulock@openss7.org>
2014-09-04 00:50:51 -0600

    started xde-keys

---
+++ src/xde-keys.c
@@ -0,0 +1,501 @@
+/*****************************************************************************
+
+ Copyright (c) 2008-2014  Monavacon Limited <http://www.monavacon.com/>
+ Copyright (c) 2001-2008  OpenSS7 Corporation <http://www.openss7.com/>
+ Copyright (c) 1997-2001  Brian F. G. Bidulock <bidulock@openss7.org>
+
+ All Rights Reserved.
+
+ This program is free software: you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the Free Software
+ Foundation, version 3 of the license.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ details.
+
+ You should have received a copy of the GNU General Public License along with
+ this program.  If not, see <http://www.gnu.org/licenses/>, or write to the
+ Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+ -----------------------------------------------------------------------------
+
+ U.S. GOVERNMENT RESTRICTED RIGHTS.  If you are licensing this Software on
+ behalf of the U.S. Government ("Government"), the following provisions apply
+ to you.  If the Software is supplied by the Department of Defense ("DoD"), it
+ is classified as "Commercial Computer Software" under paragraph 252.227-7014
+ of the DoD Supplement to the Federal Acquisition Regulations ("DFARS") (or any
+ successor regulations) and the Government is acquiring only the license rights
+ granted herein (the license rights customarily provided to non-Government
+ users).  If the Software is supplied to any unit or agency of the Government
+ other than DoD, it is classified as "Restricted Computer Software" and the
+ Government's rights in the Software are defined in paragraph 52.227-19 of the
+ Federal Acquisition Regulations ("FAR") (or any successor regulations) or, in
+ the cases of NASA, in paragraph 18.52.227-86 of the NASA Supplement to the FAR
+ (or any successor regulations).
+
+ -----------------------------------------------------------------------------
+
+ Commercial licensing and support of this software is available from OpenSS7
+ Corporation at a fee.  See http://www.openss7.com/
+
+ *****************************************************************************/
+
+#ifndef _XOPEN_SOURCE
+#define _XOPEN_SOURCE 600
+#endif
+
+#include "xde-keys.h"
+
+#ifdef _GNU_SOURCE
+#include <getopt.h>
+#endif
+
+const char *program = NAME;
+
+typedef enum {
+	CommandDefault,
+	CommandHelp,
+	CommandVersion,
+	CommandCopying,
+	CommandReplace,
+	CommandQuit,
+	CommandEdit,
+} CommandType;
+
+typedef struct {
+	int output;
+	int debug;
+	Bool dryrun;
+	CommandType command;
+	Bool replace;
+	char *display;
+	int screen;
+	Bool wait;
+	unsigned long delay;
+	char *profile;
+	char *file;
+	Bool gconf;
+	Bool write;
+} Options;
+
+Options options = {
+	.output = 1,
+	.debug = 0,
+	.dryrun = False,
+	.command = CommandDefault,
+	.replace = False,
+	.display = NULL,
+	.screen = -1,
+	.wait = True,
+	.delay = 250,
+	.profile = NULL,
+	.file = NULL,
+	.gconf = False,
+	.write = False,
+};
+
+void
+do_run(int argc, char *argv[])
+{
+}
+
+void
+do_replace(int argc, char *argv[])
+{
+}
+
+void
+do_quit(int argc, char *argv[])
+{
+}
+
+void
+do_edit(int argc, char *argv[])
+{
+}
+
+static void
+copying(int argc, char *argv[])
+{
+	if (!options.output && !options.debug)
+		return;
+	(void) fprintf(stdout, "\
+--------------------------------------------------------------------------------\n\
+%1$s\n\
+--------------------------------------------------------------------------------\n\
+Copyright (c) 2008-2014  Monavacon Limited <http://www.monavacon.com/>\n\
+Copyright (c) 2001-2008  OpenSS7 Corporation <http://www.openss7.com/>\n\
+Copyright (c) 1997-2001  Brian F. G. Bidulock <bidulock@openss7.org>\n\
+\n\
+All Rights Reserved.\n\
+--------------------------------------------------------------------------------\n\
+This program is free software: you can  redistribute it  and/or modify  it under\n\
+the terms of the  GNU Affero  General  Public  License  as published by the Free\n\
+Software Foundation, version 3 of the license.\n\
+\n\
+This program is distributed in the hope that it will  be useful, but WITHOUT ANY\n\
+WARRANTY; without even  the implied warranty of MERCHANTABILITY or FITNESS FOR A\n\
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.\n\
+\n\
+You should have received a copy of the  GNU Affero General Public License  along\n\
+with this program.   If not, see <http://www.gnu.org/licenses/>, or write to the\n\
+Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.\n\
+--------------------------------------------------------------------------------\n\
+U.S. GOVERNMENT RESTRICTED RIGHTS.  If you are licensing this Software on behalf\n\
+of the U.S. Government (\"Government\"), the following provisions apply to you. If\n\
+the Software is supplied by the Department of Defense (\"DoD\"), it is classified\n\
+as \"Commercial  Computer  Software\"  under  paragraph  252.227-7014  of the  DoD\n\
+Supplement  to the  Federal Acquisition Regulations  (\"DFARS\") (or any successor\n\
+regulations) and the  Government  is acquiring  only the  license rights granted\n\
+herein (the license rights customarily provided to non-Government users). If the\n\
+Software is supplied to any unit or agency of the Government  other than DoD, it\n\
+is  classified as  \"Restricted Computer Software\" and the Government's rights in\n\
+the Software  are defined  in  paragraph 52.227-19  of the  Federal  Acquisition\n\
+Regulations (\"FAR\")  (or any successor regulations) or, in the cases of NASA, in\n\
+paragraph  18.52.227-86 of  the  NASA  Supplement  to the FAR (or any  successor\n\
+regulations).\n\
+--------------------------------------------------------------------------------\n\
+", NAME " " VERSION);
+}
+
+static void
+version(int argc, char *argv[])
+{
+	if (!options.output && !options.debug)
+		return;
+	(void) fprintf(stdout, "\
+%1$s (OpenSS7 %2$s) %3$s\n\
+Written by Brian Bidulock.\n\
+\n\
+Copyright (c) 2008, 2009, 2010, 2011, 2012, 2013, 2014  Monavacon Limited.\n\
+Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008  OpenSS7 Corporation.\n\
+Copyright (c) 1997, 1998, 1999, 2000, 2001  Brian F. G. Bidulock.\n\
+This is free software; see the source for copying conditions.  There is NO\n\
+warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n\
+\n\
+Distributed by OpenSS7 under GNU Affero General Public License Version 3,\n\
+with conditions, incorporated herein by reference.\n\
+\n\
+See `%1$s --copying' for copying permissions.\n\
+", NAME, PACKAGE, VERSION);
+}
+
+static void
+usage(int argc, char *argv[])
+{
+	if (!options.output && !options.debug)
+		return;
+	(void) fprintf(stderr, "\
+Usage:\n\
+    %1$s [options]\n\
+    %1$s [options] {-r|--replace}\n\
+    %1$s [options] {-q|--quit}\n\
+    %1$s [options] {-e|--edit}\n\
+    %1$s [options] {-h|--help}\n\
+    %1$s {-V|--version}\n\
+    %1$s {-C|--copying}\n\
+", argv[0]);
+}
+
+const char *
+show_bool(Bool val)
+{
+	if (val)
+		return ("true");
+	return ("false");
+}
+
+static void
+help(int argc, char *argv[])
+{
+	if (!options.output && !options.debug)
+		return;
+        /* *INDENT-OFF* */
+	(void) fprintf(stdout, "\
+Usage:\n\
+    %1$s [options]\n\
+    %1$s [options] {-r|--replace}\n\
+    %1$s [options] {-q|--quit}\n\
+    %1$s [options] {-e|--edit}\n\
+    %1$s [options] {-h|--help}\n\
+    %1$s {-V|--version}\n\
+    %1$s {-C|--copying}\n\
+Command options:\n\
+    -r, --replace\n\
+        replace a running instance with the current one\n\
+    -q, --quit\n\
+        ask a running instance to quit\n\
+    -e, --edit\n\
+        launch the key binding editor\n\
+    -h, --help, -?, --?\n\
+        print this usage information and exit\n\
+    -V, --version\n\
+        print version and exit\n\
+    -C, --copying\n\
+        print copying permission and exit\n\
+General options:\n\
+    -d, --display DISPLAY		[default: %2$s]\n\
+        specify the display to use\n\
+    -s, --screen SCREEN			[default: %3$d]\n\
+        only bind to screen number SCREEN\n\
+    -n, --nowait			[default: %4$s]\n\
+        do not wait for window manager to appear\n\
+    -W, --delay DELAY			[default: %5$lu]\n\
+        wait DELAY milliseconds after window manager appears\n\
+    -P, --profile PROFILE		[default: %6$s]\n\
+        specify the window manager profile to use\n\
+    -f, --file FILE			[default: %7$s]\n\
+        use key bindings specified in resource file FILE\n\
+    -g, --gconf				[default: %8$s]\n\
+        use gconf database instead of resource file\n\
+    -w, --write				[default: %9$s]\n\
+        write operational key bindings to gconf\n\
+    -v, --verbose [LEVEL]		[default: %10$d]\n\
+        increment or set output verbosity LEVEL\n\
+        this option may be repeated.\n\
+    -D, --debug [LEVEL]			[default: %11$d]\n\
+        increment or set debug LEVEL\n\
+"	,argv[0]
+	,options.display
+	,options.screen
+	,show_bool(options.wait)
+	,options.delay
+	,options.profile
+	,options.file
+	,show_bool(options.gconf)
+	,show_bool(options.write)
+	,options.output
+	,options.debug
+	);
+        /* *INDENT-ON* */
+}
+
+void
+set_defaults(int argc, char *argv[])
+{
+	char *p;
+
+	if ((p = getenv("XDE_DEBUG")))
+		options.debug = atoi(p);
+	if ((p = getenv("DISPLAY")))
+		options.display = strdup(p);
+}
+
+void
+get_defaults(int argc, char *argv[])
+{
+}
+
+int
+main(int argc, char *argv[])
+{
+	CommandType command = CommandDefault;
+
+	setlocale(LC_ALL, "");
+
+	set_defaults(argc, argv);
+
+	while (1) {
+		int c, val;
+
+#ifdef _GNU_SOURCE
+		int option_index = 0;
+		/* *INDENT-OFF* */
+		static struct option long_options[] = {
+			{"replace",	no_argument,		NULL, 'r'},
+			{"quit",	no_argument,		NULL, 'q'},
+			{"edit",	no_argument,		NULL, 'e'},
+
+			{"display",	required_argument,	NULL, 'd'},
+			{"screen",	required_argument,	NULL, 's'},
+			{"nowait",	no_argument,		NULL, 'n'},
+			{"delay",	required_argument,	NULL, 'W'},
+			{"profile",	required_argument,	NULL, 'P'},
+			{"file",	required_argument,	NULL, 'F'},
+			{"gconf",	no_argument,		NULL, 'g'},
+			{"write",	no_argument,		NULL, 'w'},
+
+			{"clientId",	required_argument,	NULL, '0'},
+			{"saveFile",	required_argument,	NULL, '1'},
+
+			{"debug",	optional_argument,	NULL, 'D'},
+			{"verbose",	optional_argument,	NULL, 'v'},
+			{"help",	no_argument,		NULL, 'h'},
+			{"version",	no_argument,		NULL, 'V'},
+			{"copying",	no_argument,		NULL, 'C'},
+			{"?",		no_argument,		NULL, 'H'},
+			{ 0, }
+		};
+		/* *INDENT-ON* */
+
+		c = getopt_long_only(argc, argv, "D::v::hVCH?", long_options, &option_index);
+#else				/* _GNU_SOURCE */
+		c = getopt(argc, argv, "DvhVC?");
+#endif				/* _GNU_SOURCE */
+		if (c == -1) {
+			if (options.debug)
+				fprintf(stderr, "%s: done options processing\n", argv[0]);
+			break;
+		}
+		switch (c) {
+		case 0:
+			goto bad_usage;
+
+		case 'r':	/* -r, --replace */
+			if (options.command != CommandDefault)
+				goto bad_option;
+			if (command == CommandDefault)
+				command = CommandReplace;
+			options.command = CommandReplace;
+			options.replace = True;
+			break;
+		case 'q':	/* -q, --quit */
+			if (options.command != CommandDefault)
+				goto bad_option;
+			if (command == CommandDefault)
+				command = CommandQuit;
+			options.command = CommandQuit;
+			options.replace = True;
+			break;
+		case 'e':	/* -e, --edit */
+			if (options.command != CommandDefault)
+				goto bad_option;
+			if (command == CommandDefault)
+				command = CommandEdit;
+			options.command = CommandEdit;
+			options.replace = False;
+			break;
+
+		case 'd':	/* -d, --display DISPLAY */
+			setenv("DISPLAY", optarg, True);
+			break;
+		case 's':	/* -s, --screen SCREEN */
+			options.screen = atoi(optarg);
+			break;
+		case 'n':	/* -n, --nowait */
+			options.wait = False;
+			break;
+		case 'W':	/* -W, --delay DELAY */
+			options.delay = strtoul(optarg, NULL, 0);
+			break;
+		case 'P':	/* -P, --profile PROFILE */
+			options.profile = strdup(optarg);
+			break;
+		case 'f':	/* -f, --file FILE */
+			options.file = strdup(optarg);
+			break;
+		case 'g':	/* -g, --gconf */
+			options.gconf = True;
+			break;
+		case 'w':	/* -w, --write */
+			options.write = True;
+			break;
+
+		case 'D':	/* -D, --debug [level] */
+			if (options.debug)
+				fprintf(stderr, "%s: increasing debug verbosity\n", argv[0]);
+			if (optarg == NULL) {
+				options.debug++;
+			} else {
+				if ((val = strtol(optarg, NULL, 0)) < 0)
+					goto bad_option;
+				options.debug = val;
+			}
+			break;
+		case 'v':	/* -v, --verbose [level] */
+			if (options.debug)
+				fprintf(stderr, "%s: increasing output verbosity\n", argv[0]);
+			if (optarg == NULL) {
+				options.output++;
+				break;
+			}
+			if ((val = strtol(optarg, NULL, 0)) < 0)
+				goto bad_option;
+			options.output = val;
+			break;
+		case 'h':	/* -h, --help */
+		case 'H':	/* -H, --? */
+			command = CommandHelp;
+			break;
+		case 'V':	/* -V, --version */
+			if (options.command != CommandDefault)
+				goto bad_option;
+			if (command == CommandDefault)
+				command = CommandVersion;
+			options.command = CommandVersion;
+			break;
+		case 'C':	/* -C, --copying */
+			if (options.command != CommandDefault)
+				goto bad_option;
+			if (command == CommandDefault)
+				command = CommandCopying;
+			options.command = CommandCopying;
+			break;
+		case '?':
+		default:
+		      bad_option:
+			optind--;
+			goto bad_nonopt;
+		      bad_nonopt:
+			if (options.output || options.debug) {
+				if (optind < argc) {
+					fprintf(stderr, "%s: syntax error near '", argv[0]);
+					while (optind < argc) {
+						fprintf(stderr, "%s", argv[optind++]);
+						fprintf(stderr, "%s", (optind < argc) ? " " : "");
+					}
+					fprintf(stderr, "'\n");
+				} else {
+					fprintf(stderr, "%s: missing option or argument", argv[0]);
+					fprintf(stderr, "\n");
+				}
+				fflush(stderr);
+			      bad_usage:
+				usage(argc, argv);
+			}
+			exit(2);
+		}
+	}
+	DPRINTF("%s: option index = %d\n", argv[0], optind);
+	DPRINTF("%s: option count = %d\n", argv[0], argc);
+	get_defaults(argc, argv);
+	if (optind < argc) {
+		fprintf(stderr, "%s: excess non-options arguments near '", argv[0]);
+		goto bad_nonopt;
+	}
+	switch (command) {
+	default:
+	case CommandDefault:
+		DPRINTF("%s: running default\n", argv[0]);
+		do_run(argc, argv);
+		exit(EXIT_FAILURE);
+		break;
+	case CommandReplace:
+		DPRINTF("%s: running replace\n", argv[0]);
+		do_run(argc, argv);
+		break;
+	case CommandQuit:
+		DPRINTF("%s: running quit\n", argv[0]);
+		do_quit(argc, argv);
+		break;
+	case CommandEdit:
+		DPRINTF("%s: running edit\n", argv[0]);
+		do_edit(argc, argv);
+		break;
+	case CommandHelp:
+		DPRINTF("%s: printing help message\n", argv[0]);
+		help(argc, argv);
+		break;
+	case CommandVersion:
+		DPRINTF("%s: printing version message\n", argv[0]);
+		version(argc, argv);
+		break;
+	case CommandCopying:
+		DPRINTF("%s: printing copying message\n", argv[0]);
+		copying(argc, argv);
+		break;
+	}
+	exit(EXIT_SUCCESS);
+}
