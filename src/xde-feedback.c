1c9a1098c443b6ddce5e0f8a2a441049e0d3cd75
Brian Bidulock <bidulock@openss7.org>
2016-07-31 01:11:10 -0600

    more rationalization

--- src/xde-feedback.c
+++ src/xde-feedback.c
@@ -45,12 +45,12 @@
 /** @section Headers
   * @{ */
 
-#ifdef HAVE_CONFIG_H
-#include "autoconf.h"
-#endif
-
 #ifndef _XOPEN_SOURCE
 #define _XOPEN_SOURCE 600
+#endif
+
+#ifdef HAVE_CONFIG_H
+#include "autoconf.h"
 #endif
 
 #include <stddef.h>
