fb7720d31721aefe8e44dbd00e88879382f0c1d8
Brian Bidulock <bidulock@openss7.org>
2018-06-18 22:10:43 -0600

    Revert "glibc keeps breaking optional_argument behaviour"
    This reverts commit d1140aa7affe8e8a6fe2beaa60f0f3a14fdc59de.

--- src/xde-run.c
+++ src/xde-run.c
@@ -1557,8 +1557,6 @@ main(int argc, char *argv[])
 	}
 	DPRINTF("%s: option index = %d\n", argv[0], optind);
 	DPRINTF("%s: option count = %d\n", argv[0], argc);
-#if 0
-	/* glibc keeps breaking this for optional arguments */
 	if (optind < argc) {
 		fprintf(stderr, "%s: excess non-option arguments near '", argv[0]);
 		while (optind < argc) {
@@ -1569,7 +1567,6 @@ main(int argc, char *argv[])
 		usage(argc, argv);
 		exit(EXIT_SYNTAXERR);
 	}
-#endif
 	switch (command) {
 	default:
 	case CommandDefault:
