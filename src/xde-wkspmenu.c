9d0f3688e057f3b9a837549bd8727eceb318d59e
Brian Bidulock <bidulock@openss7.org>
2018-06-18 21:49:22 -0600

    glibc keeps breaking optional_argument behaviour

--- src/xde-wkspmenu.c
+++ src/xde-wkspmenu.c
@@ -8138,6 +8138,8 @@ main(int argc, char *argv[])
 	}
 	DPRINTF(1, "%s: option index = %d\n", argv[0], optind);
 	DPRINTF(1, "%s: option count = %d\n", argv[0], argc);
+#if 0
+	/* glibc keeps breaking this for optional arguments */
 	if (optind < argc) {
 		EPRINTF("excess non-option arguments near '");
 		while (optind < argc) {
@@ -8148,6 +8150,7 @@ main(int argc, char *argv[])
 		usage(argc, argv);
 		exit(EXIT_SYNTAXERR);
 	}
+#endif
 	startup(argc, argv);
 	get_defaults();
 
