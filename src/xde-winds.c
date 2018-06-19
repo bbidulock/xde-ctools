b0a894e2358feec5cecdd29420f4d10380bc1e4f
Brian Bidulock <bidulock@openss7.org>
2018-06-18 21:49:00 -0600

    glibc keeps breaking optional_argument behaviour

--- src/xde-winds.c
+++ src/xde-winds.c
@@ -8823,6 +8823,8 @@ main(int argc, char *argv[])
 	}
 	DPRINTF(1, "%s: option index = %d\n", argv[0], optind);
 	DPRINTF(1, "%s: option count = %d\n", argv[0], argc);
+#if 0
+	/* glibc keeps breaking this for optional arguments */
 	if (optind < argc) {
 		EPRINTF("excess non-option arguments near '");
 		while (optind < argc) {
@@ -8833,6 +8835,7 @@ main(int argc, char *argv[])
 		usage(argc, argv);
 		exit(EXIT_SYNTAXERR);
 	}
+#endif
 	startup(argc, argv);
 	get_defaults();
 
